98ea354dc70d13dc691db4a622edf84704fb7df8
Mark Mazur <mark.mazur007@gmail.com>
2024-05-13 14:36:19 -0400

    edit five_generator.py

--- nets/five_generator.py
+++ nets/five_generator.py
@@ -1,32 +1,29 @@
+import subprocess
+import sys
 import os
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
-from torch import optim
 
-def init_variables() -> None:
-    global device, nz, ngf, ndf, nc, dataloader
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    transform = transforms.Compose([
-        transforms.Resize(64),
-        transforms.CenterCrop(64),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+transform = transforms.Compose([
+    transforms.Resize(64),
+    transforms.CenterCrop(64),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
-    dataset = datasets.ImageFolder(root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'five_generator'))
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-    nz, ngf, ndf, nc = 100, 64, 64, 3
+dataset = datasets.ImageFolder(root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'five_generator'), transform=transform)
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
 
 
 class Generator(nn.Module):
-    def __init__(self, nz: int, ngf: int, nc: int):
+    def __init__(self, nz, ngf, nc):
         super(Generator, self).__init__()
-
         self.main = nn.Sequential(
             nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
@@ -60,21 +57,27 @@ class Discriminator(nn.Module):
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ndf * 4, 1, 4, 1, 0, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(ndf * 8, 1, 1),
             nn.Sigmoid()
         )
 
     def forward(self, input):
-        return self.main(input)
+        return self.main(input).view(-1)
 
 
-def create_modules() -> None:
-    global netG, netD
+nz = 100
+ngf = 64
+ndf = 64
+nc = 3
 
-    netG = Generator(nz, ngf, nc).to(device)
-    netD = Generator(nz, ngf, nc).to(device)
+netG = Generator(nz, ngf, nc).to(device)
+netD = Discriminator(ndf, nc).to(device)
 
-def weights_init(m) -> None:
+def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
@@ -82,74 +85,66 @@ def weights_init(m) -> None:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-def apply_weights() -> None:
-    netG.apply(weights_init)
-    netD.apply(weights_init)
+netG.apply(weights_init)
+netD.apply(weights_init)
 
-def optimizer() -> None:
-    global optimizerG, optimizerD, criterion
+optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
-    optimizerD = optim.Adam(netD.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    optimizerG = optim.Adam(netG.parameters(), lr=0.0002, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
 
-    criterion = nn.BCELoss()
+num_epochs = 13500
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader, 0):
+        netD.zero_grad()
+        real = data[0].to(device)
+        b_size = real.size(0)
+        label = torch.full((b_size,), 1, dtype=torch.float, device=device)
 
-def learn(*, num_epochs: int) -> None:
-    for epoch in range(num_epochs):
-        for i, data in enumerate(dataloader, 0):
-            netD.zero_grad()
-            real = data[0].to(device)
-            b_size = real.size(0)
-            label = torch.full((b_size,), 1, dtype=torch.float, device=device)
+        output = netD(real).view(-1)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
 
-            output = netD(real).view(-1)
-            errD_real = criterion(output, label)
-            errD_real.backward()
-            D_x = output.mean().item()
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        fake = netG(noise)
+        label.fill_(0)
+        output = netD(fake.detach()).view(-1)
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        errD = errD_real + errD_fake
+        optimizerD.step()
 
-            noise = torch.randn(b_size, nz, 1, 1, device=device)
-            fake = netG(noise)
-            label.fill_(0)
-            output = netD(fake.detach()).view(-1)
-            errD_fake = criterion(output, label)
-            errD_fake.backward()
-            D_G_z1 = output.mean().item()
-            errD = errD_real + errD_fake
-            optimizerD.step()
+        netG.zero_grad()
+        label.fill_(1)
+        output = netD(fake).view(-1)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        optimizerG.step()
 
-            netG.zero_grad()
-            label.fill_(1)
-            output = netD(fake).view(-1)
-            errG = criterion(output, label)
-            errG.backward()
-            D_G_z2 = output.mean().item()
-            optimizerG.step()
+    print(f'Epoch {epoch + 1}/{num_epochs} [{i + 1}/{len(dataloader)}] Loss_D: {errD.item():.4f} Loss_G: {errG.item():.4f} D(x): {D_x:.4f} D(G(z1)): {D_G_z1:.4f} D(G(z2)): {D_G_z2:.4f}')
 
-        print(f'Epoch {epoch + 1}/{num_epochs} [{i + 1}/{len(dataloader)}] Loss_D: {errD.item():.4f} Loss_G: {errG.item():.4f} D(x): {D_x:.4f} D(G(z1)): {D_G_z1:.4f} D(G(z2)): {D_G_z2:.4f}')
+with torch.no_grad():
+    noise = torch.randn(1, nz, 1, 1, device=device)
+    fake = netG(noise).detach().cpu()
 
-def show_result() -> None:
-    with torch.no_grad():
-        noise = torch.randn(1, nz, 1, 1, device=device)
-        fake = netG(noise).detach().cpu()
+plt.figure(figsize=(5,5))
+plt.axis("off")
+plt.title("Generated Image")
+plt.imshow(fake[0].permute(1, 2, 0) * 0.5 + 0.5)
+plt.show()
+plt.savefig(f'{os.path.dirname(os.path.abspath(__file__))}/modules/five_generator/5.png')
 
-    plt.figure(figsize=(5, 5))
-    plt.axis("off")
-    plt.title("Generated Image")
-    plt.imshow(fake[0].permute(1, 2, 0) * 0.5 + 0.5)
-    plt.show()
+name_of_modules = ('_35000ep_64x64', '_35000ep_64x64')
 
-def save_modules(*, should_save_module: bool, name_of_modules: tuple[str, str] = ('', '')) -> None:
-    if not should_save_module: return
+torch.save(netG, f'{os.path.dirname(os.path.abspath(__file__))}/modules/five_generator/NetG{name_of_modules[0]}.pth')
+torch.save(netD, f'{os.path.dirname(os.path.abspath(__file__))}/modules/five_generator/NetD{name_of_modules[1]}.pth')
 
-    torch.save(netG, f'{os.path.dirname(os.path.abspath(__file__))}/modules/five_generator/NetG{name_of_modules[0]}.pth')
-    torch.save(netD, f'{os.path.dirname(os.path.abspath(__file__))}/modules/five_generatorNetD{name_of_modules[1]}.pth')
-
-def main() -> None:
-    init_variables()
-    create_modules()
-    apply_weights()
-    optimizer()
-    learn(num_epochs=2)
-
-if __name__ == '__main__':
-    main()
+try:
+    subprocess.run(['shutdown', '-h', 'now'])
+except subprocess.CalledProcessError:
+    pass
+finally:
+    sys.exit()
